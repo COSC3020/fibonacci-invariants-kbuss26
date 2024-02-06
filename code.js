bd679e119c0f790cd6a0833f176e6f496ec2ce25
Keifer <144736945+kbuss26@users.noreply.github.com>
2024-02-05 21:08:53 -0700

    Solution #1 of code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function fib(n) {
+    switch (n){
+        case 0:
+            return [0];
+        case 1:
+            return [0,1];
+        default:
+            var array = fib(n - 1);
+            array.push(array[n - 2] + array[n - 1]);
+            return array;
+    }
     return n;
 }
